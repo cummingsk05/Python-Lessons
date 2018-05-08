f165e9e72b5f87edd37bd6c60d462ffdc7d83c27
cummingsk05 <cummingsk05@gmail.com>
2018-05-07 22:40:17 -0400

    journal banner

---
+++ apps/04_journal/you_try/project.py
@@ -0,0 +1,11 @@
+def print_banner():
+    print()
+    print('---------------------------')
+    print('   Personal Journal App')
+    print('---------------------------')
+    print()
+print_banner()
+
+def main():
+    print_banner()
+
